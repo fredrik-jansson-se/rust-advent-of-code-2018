b48f48fa03604a57732af3def0eb3034433b6180
Fredrik Jansson <frjansso@cisco.com>
2018-12-09 21:10:54 -0800

    Day3

---
+++ src/aoc3.rs
@@ -0,0 +1,111 @@
+use regex::Regex;
+use std::fs;
+use std::collections::HashMap;
+
+pub fn run() {
+    let input = fs::read_to_string("day3.txt").unwrap();
+
+    println!("3:1 - {}", run_1(&input));
+    println!("3:2 - {}", run_2(&input));
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct Claim {
+    id: u32,
+    x: u32,
+    y: u32,
+    width: u32,
+    height: u32
+}
+
+impl Claim {
+    fn new(id: u32, x: u32, y: u32, w: u32, h: u32) -> Claim {
+        Claim { id: id, x: x, y: y, width: w, height: h }
+    }
+}
+
+fn s2i(s: &str) -> u32 {
+    u32::from_str_radix(s, 10).unwrap()
+}
+
+fn parse(row: &str) -> Claim {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r#"#(\d+)\s+@\s+(\d+),(\d+):\s+(\d+)x(\d+)"#).unwrap();
+    }
+
+    let c = RE.captures(row).unwrap();
+    Claim::new(s2i(&c[1]), s2i(&c[2]), s2i(&c[3]), s2i(&c[4]), s2i(&c[5]))
+}
+
+fn run_1(input: &str) -> u32 {
+    let components : Vec<Claim> = input.lines().map(parse).collect();
+    let mut clms = HashMap::new();
+
+    for c in components {
+        for x in c.x..(c.x+c.width) {
+            for y in c.y..(c.y+c.height) {
+                let counter = clms.entry((x,y)).or_insert(0);
+                *counter += 1;
+            }
+        }
+    }
+    let mut cnt = 0;
+    for (_, v) in clms {
+        if v > 1 {
+            cnt += 1;
+        }
+    }
+    cnt
+}
+
+fn run_2(input: &str) -> u32 {
+    let components : Vec<Claim> = input.lines().map(parse).collect();
+    let mut clms = HashMap::new();
+
+    for c in components.iter() {
+        for x in c.x..(c.x+c.width) {
+            for y in c.y..(c.y+c.height) {
+                let counter = clms.entry((x,y)).or_insert(0);
+                *counter += 1;
+            }
+        }
+    }
+
+    for c in components.iter() {
+        let mut found = false;
+        for x in c.x..(c.x+c.width) {
+            for y in c.y..(c.y+c.height) {
+                if *clms.get(&(x,y)).unwrap() > 1 {
+                    found = true;
+                    break;
+                }
+            }
+            if found {
+                break;
+            }
+        }
+        if !found {
+            return c.id;
+        }
+    }
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn aoc3_parse() {
+        assert_eq!(Claim::new(1, 662, 777, 18, 27), parse("#1 @ 662,777: 18x27"));
+    }
+
+    #[test]
+    fn aoc3_run_1() {
+        assert_eq!(4, run_1("#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2"));
+    }
+
+    #[test]
+    fn aoc3_run_2() {
+        assert_eq!(3, run_2("#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2"));
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+extern crate regex;
+#[macro_use] extern crate lazy_static;
+
+use std::env;
+
+// mod aoc1;
+// mod aoc2;
+mod aoc3;
+// mod aoc16;
+// mod aoc18;  
+// mod aoc19;
+// mod aoc20;
+// mod aoc21;
+// mod aoc22;
+// mod aoc23;
+// mod aoc24;
+// mod aoc25;
+
+fn main() {
+    let mut a = env::args();
+    a.next();
+
+    let day = match a.next() {
+        Some(s) => usize::from_str_radix(&s, 10).unwrap(),
+        None => 0,
+    };
+
+    match day {
+        // 1 => aoc1::run(),
+        // 2 => aoc2::run(),
+        3 => aoc3::run(),
+        // 16 => aoc16::run(),
+        // 18 => aoc18::run(),
+        // 19 => aoc19::run(),
+        // 20 => aoc20::run(),
+        // 21 => aoc21::run(),
+        // 22 => aoc22::run(),
+        // 23 => aoc23::run(),
+        // 24 => aoc24::run(),
+        // 25 => aoc25::run(),
+        _ => ()
+    }
+}
