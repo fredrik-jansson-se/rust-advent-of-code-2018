012fe8dbed45c87ca98faef24f2e078ca25d969c
Fredrik Jansson <frjansso@cisco.com>
2019-02-20 15:33:56 -0800

    Fixed negative number parsing

--- src/helper.rs
+++ src/helper.rs
@@ -1,8 +1,16 @@
 use nom::types::CompleteStr;
 use nom::*;
 
+
+fn signed_to_val(s: (Option<CompleteStr>, CompleteStr)) -> Result<i32, std::num::ParseIntError> {
+    match s.0 {
+        Some(CompleteStr("-")) => s.1.parse::<i32>().map(|v| -v),
+        _ => s.1.parse::<i32>(),
+    }
+}
+
 named!(pub i32_val<CompleteStr, i32>,
-       map_res!(digit, |s: CompleteStr| s.parse::<i32>()));
+       map_res!(pair!(opt!(alt!(tag!("-") | tag!("+"))),  digit), signed_to_val));
 
 named!(pub u32_val<CompleteStr, u32>,
        map_res!(digit, |s: CompleteStr| { s.parse::<u32>()} )
@@ -14,6 +22,7 @@ mod tests {
     #[test]
     fn helper_parse_int32() {
         assert_eq!(i32_val(CompleteStr("123")), Ok((CompleteStr(""), 123)));
+        assert_eq!(i32_val(CompleteStr("+123")), Ok((CompleteStr(""), 123)));
         assert_eq!(i32_val(CompleteStr("-123")), Ok((CompleteStr(""), -123)));
     }
 
